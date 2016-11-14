a3893a02474055883fcacc4567a565557b1c7ce0
Gourav Sharma <gouravsharma2006@gmail.com>
2016-11-14 15:17:03 +0530

    refactoring to exclude small file sizes

--- gzip.js
+++ gzip.js
@@ -1,16 +1,28 @@
 const fs   = require( 'fs' );
 const path = require( 'path' );
+const zlib = require( 'zlib' );
 
 const gzipSize = require( 'gzip-size' );
 
 const fromRoot = fileName => path.join( __dirname, fileName ); 
 
 const distFolder = fs.readdirSync( './dist/' );
+const gzip       = zlib.createGzip();
 
 for ( let i in distFolder ) {
+    if ( Math.ceil( gzipSize.sync( fs.readFileSync( fromRoot( `dist/${distFolder[i]}` ) ).toString() ) / 1024 ) >= 102.4 ) {
+        if ( !fromRoot( `dist/${distFolder[i]}` ).endsWith( '.gz' ) ) {
 console.log( `---------------------------
 File : ${distFolder[i]}
 ---------------------------
-File Size Before Gzip : ~${Math.ceil( fs.readFileSync( fromRoot( `dist/${distFolder[i]}` ) ).toString().length / 1024 )}-KB
-File Size After  Gzip : ~${Math.ceil( gzipSize.sync( fs.readFileSync( fromRoot( `dist/${distFolder[i]}` ) ).toString() ) / 1024 )}-KB` );
+Size Before Gzip : ~${Math.ceil( fs.readFileSync( fromRoot( `dist/${distFolder[i]}` ) ).toString().length / 1024 )}-KB
+Size After  Gzip : ~${Math.ceil( gzipSize.sync( fs.readFileSync( fromRoot( `dist/${distFolder[i]}` ) ).toString() ) / 1024 )}-KB` );
+
+        
+            // fs.createReadStream( fromRoot( `dist/${distFolder[i]}` ) )
+            //     .pipe( gzip )
+            //     .pipe( fs.createWriteStream( fromRoot( `dist/${distFolder[i]}.gz` ) ) );
+        }
+
+    }
 }
